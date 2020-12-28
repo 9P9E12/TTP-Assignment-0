86cf59cc83ba4b16f79ed096bb038ccc778565e1
Justin Goodchild <jeg6300@gmail.com>
2020-12-28 13:48:26 -0500

    Wrote & tested countOfAllBooleans.js

--- 04-countOfAllBooleans/countOfAllBooleans.js
+++ 04-countOfAllBooleans/countOfAllBooleans.js
@@ -1,5 +1,11 @@
 function countOfAllBooleans(arr) {
-  // Insert code here;
+  var bools = 0;
+  for (var i = 0; i < arr.length; i++) {
+    if(typeof arr[i] === "boolean"){
+      bools++;
+    }
+  }
+  return bools;
 }
 
 // Do not edit this line;
