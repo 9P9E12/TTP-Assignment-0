e8daaf572a23babfcf5f954709bb75db3b630b76
Justin Goodchild <jeg6300@gmail.com>
2020-12-28 14:13:40 -0500

    Wrote & tested fizzBuzz.js

--- 12-fizzBuzz/fizzBuzz.js
+++ 12-fizzBuzz/fizzBuzz.js
@@ -1,5 +1,19 @@
 function fizzBuzz(start, end) {
-  // Insert code here;
+  var output = [];
+  for(var i = start; i <= end; i++){
+    var result = "";
+    if(i % 3 === 0){
+        result = result.concat("Fizz");
+    }
+    if(i % 5 === 0){
+        result = result.concat("Buzz");
+    }
+    if(result.length === 0){
+        result = i;
+    }
+    output.push(result);
+  }
+  return output;
 }
 
 // Do not edit this line;
