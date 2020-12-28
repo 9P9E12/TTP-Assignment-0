489fa3fbe44115b521173e2f5e6269d3697a7749
Justin Goodchild <jeg6300@gmail.com>
2020-12-28 16:47:04 -0500

    Wrote & tested frequencyCounter.js

--- 14-frequencyCounter/frequencyCounter.js
+++ 14-frequencyCounter/frequencyCounter.js
@@ -1,5 +1,14 @@
 function frequencyCounter(word) {
-  // Insert code here;
+  var frequency = new Object();
+  for (var i = 0; i < word.length; i++) {
+    if(word[i] in frequency){
+      var prev = frequency[word[i]];
+      frequency[word[i]] = prev+1;
+    } else {
+      frequency[word[i]] = 1;
+    }
+  }
+  return frequency;
 }
 
 // Do not edit this line;
