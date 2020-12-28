2d525483363ee1e0ca1e073e15a772e7a3787cf2
Justin Goodchild <jeg6300@gmail.com>
2020-12-28 17:00:15 -0500

    Wrote & tested setUnionOfAnyAmountOfSets.js

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,5 +1,9 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  // Insert code here;
+  var union = new Set();
+  args.forEach(element => {
+    element.forEach(subElement => union.add(subElement));
+  });
+  return union
 }
 
 // Do not edit this line;
