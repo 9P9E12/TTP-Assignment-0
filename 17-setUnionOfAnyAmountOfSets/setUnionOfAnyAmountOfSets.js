4f246c98b5db2e7e1852ab78a27a06ca3d86e17d
Justin Goodchild <jeg6300@gmail.com>
2020-12-28 17:01:06 -0500

    Added comments to setUnionOfAnyAmountOfSets.js for future reference if I ever revisit this file

--- 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
+++ 17-setUnionOfAnyAmountOfSets/setUnionOfAnyAmountOfSets.js
@@ -1,7 +1,7 @@
 function setUnionOfAnyAmountOfSets(...args) {
-  var union = new Set();
-  args.forEach(element => {
-    element.forEach(subElement => union.add(subElement));
+  var union = new Set(); //Make set that will contain every item
+  args.forEach(element => { //Get a set from the argument list
+    element.forEach(subElement => union.add(subElement)); //Get an element in the set from the previous line and add it to the new set
   });
   return union
 }
