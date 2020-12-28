b3d6a022eea51c9a55e77ba72c29d6e54e7632a0
Justin Goodchild <jeg6300@gmail.com>
2020-12-28 13:51:06 -0500

    Wrote & tested countOfAllNumbersSmallerThanTarget.js

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,11 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  var smaller = 0;
+  for (var i = 0; i < nums.length; i++) {
+    if(nums[i] < target){
+      smaller++;
+    }
+  }
+  return smaller;
 }
 
 // Do not edit this line;
